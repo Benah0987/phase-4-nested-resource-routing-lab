1d19749e7307bab3c4d3a10fe9af3db178e27608
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-27 13:19:10 +0300

    nested routing and usages

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,25 @@ class ItemsController < ApplicationController
     items = Item.all
     render json: items, include: :user
   end
+  # creating a show action
+  def show
+    item = Item.find_by(id: params[:id], user_id: params[:user_id])
+    render json: item
+  end
+  # creating a create action
+  def create
+    user = User.find_by(id: params[:user_id])
+    item = user.items.build(item_params)
+    if item.save
+      render json: item, status: :created
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
 end
